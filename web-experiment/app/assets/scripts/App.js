0c6b8a73cc2cc5a666eaf4c24cd4fc38100fe2bd
simulaterz <ronnakorn.suw@gmail.com>
2017-02-28 23:28:19 +0700

    add toggleClass to switch class from menuContent

--- web-experiment/app/assets/scripts/App.js
+++ web-experiment/app/assets/scripts/App.js
@@ -1,20 +1,3 @@
-var $ = require('jquery');
-// var Person = require('./modules/Person'); // NodeJS
-import Person from './modules/Person'; // ES6
+import MobileMenu from './modules/MobileMenu';
 
-alert("ABC 002");
-
-class Adult extends Person {
-  payTaxes() {
-    console.log(this.name + " now owes $0 in taxes");
-  }
-}
-
-var john = new Person("John Doe", "Blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "Light Green");
-jane.greet();
-jane.payTaxes();
-
-$('h1').remove();
+var mobileMenu = new MobileMenu();
