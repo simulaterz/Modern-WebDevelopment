c50b222a7e0c5a69ebb38d01cba1d1ea3bbbd01d
simulaterz <ronnakorn.suw@gmail.com>
2017-03-01 21:14:11 +0700

    add edit side-header when clicked

--- web-experiment/app/assets/scripts/modules/MobileMenu.js
+++ web-experiment/app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 
 class MobileMenu {
   constructor() {
+    this.siteHeader = $(".site-header");
     this.menuIcon = $(".site-header__menu-icon");
     this.menuContent = $(".site-header__menu-content");
     this.events();
@@ -9,11 +10,11 @@ class MobileMenu {
 
   events() {
     this.menuIcon.click(this.toggleTheMenu.bind(this));
-    console.log(this);
   }
 
   toggleTheMenu() {
     this.menuContent.toggleClass("site-header__menu-content--is-visible");
+    this.siteHeader.toggleClass("site-header--is-expanded");
   }
 }
 
--- web-experiment/app/temp/scripts/App.js
+++ web-experiment/app/temp/scripts/App.js
@@ -91,6 +91,7 @@ var MobileMenu = function () {
   function MobileMenu() {
     _classCallCheck(this, MobileMenu);
 
+    this.siteHeader = (0, _jquery2.default)(".site-header");
     this.menuIcon = (0, _jquery2.default)(".site-header__menu-icon");
     this.menuContent = (0, _jquery2.default)(".site-header__menu-content");
     this.events();
@@ -100,12 +101,12 @@ var MobileMenu = function () {
     key: "events",
     value: function events() {
       this.menuIcon.click(this.toggleTheMenu.bind(this));
-      console.log(this);
     }
   }, {
     key: "toggleTheMenu",
     value: function toggleTheMenu() {
       this.menuContent.toggleClass("site-header__menu-content--is-visible");
+      this.siteHeader.toggleClass("site-header--is-expanded");
     }
   }]);
 
